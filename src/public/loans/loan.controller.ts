377e9cec6ba02571df7fa183a7413ab7a7d6ea51
Kacelo <vernonkachelo@gmail.com>
2024-05-31 11:37:12 +0200

    added loan fetching for lender

--- src/public/loans/loan.controller.ts
+++ src/public/loans/loan.controller.ts
@@ -1,60 +1,91 @@
 // loan.controller.ts
-import { Body, Controller, Param, Patch, Post, Res, HttpStatus, Get } from '@nestjs/common';
-import { LoanService } from './loan.service';
-import { CreateLoanDto } from './dto/createLoanDto';
-import { UpdateLoanDto } from './dto/updateLoanDto';
-import mongoose from 'mongoose';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  Res,
+  HttpStatus,
+  Get,
+} from "@nestjs/common";
+import { LoanService } from "./loan.service";
+import { CreateLoanDto } from "./dto/createLoanDto";
+import { UpdateLoanDto } from "./dto/updateLoanDto";
+import mongoose from "mongoose";
 const { ObjectId } = mongoose.Types;
 
-
-@Controller('loans')
+@Controller("loans")
 export class LoanController {
   constructor(private readonly loanService: LoanService) {}
 
-  @Post('create')
+  @Post("create")
   async createLoan(@Res() response, @Body() createLoanDto: CreateLoanDto) {
     try {
       const newLoan = await this.loanService.createLoan(createLoanDto);
       return response.status(HttpStatus.CREATED).json(newLoan);
     } catch (err) {
       return response.status(HttpStatus.BAD_REQUEST).json({
-        message: 'Error: Loan not created!',
+        message: "Error: Loan not created!",
         error: err.message,
       });
     }
   }
 
-  @Patch('update/:id')
-  async updateLoan(@Res() response, @Param('id') id: string, @Body() updateLoanDto: UpdateLoanDto) {
+  @Patch("update/:id")
+  async updateLoan(
+    @Res() response,
+    @Param("id") id: string,
+    @Body() updateLoanDto: UpdateLoanDto
+  ) {
     try {
       const updatedLoan = await this.loanService.updateLoan(id, updateLoanDto);
       return response.status(HttpStatus.OK).json(updatedLoan);
     } catch (err) {
       return response.status(HttpStatus.BAD_REQUEST).json({
-        message: 'Error: Loan not updated!',
+        message: "Error: Loan not updated!",
         error: err.message,
       });
     }
   }
-  @Patch('delete/:id')
-  async deleteLoan(@Res() response, @Param('id') id: string, @Body() updateLoanDto: UpdateLoanDto) {
+  @Patch("delete/:id")
+  async deleteLoan(
+    @Res() response,
+    @Param("id") id: string,
+    @Body() updateLoanDto: UpdateLoanDto
+  ) {
     try {
-        const convertedId = updateLoanDto.deleted
-        const updatedLoan = await this.loanService.deleteLoan(id, convertedId);
-        return response.status(HttpStatus.OK).json(updatedLoan);
-      } catch (err) {
-        return response.status(HttpStatus.BAD_REQUEST).json({
-          message: 'Error: Loan not updated!',
-          error: err.message,
-        });
-      }
+      const convertedId = updateLoanDto.deleted;
+      const updatedLoan = await this.loanService.deleteLoan(id);
+      return response.status(HttpStatus.OK).json(updatedLoan);
+    } catch (err) {
+      return response.status(HttpStatus.BAD_REQUEST).json({
+        message: "Error: Loan not updated!",
+        error: err.message,
+      });
+    }
   }
   @Get()
   async getLoans(@Res() response) {
     try {
       const loanData = await this.loanService.getAllLoans();
       return response.status(HttpStatus.OK).json({
-        message: 'All loan data found successfullyy',
+        message: "All loan data found successfullyy",
+        loanData,
+      });
+    } catch (err) {
+      return response.status(err.status).json(err.response);
+    }
+  }
+  @Get("applications/:lenderId")
+  async getLoanApplications(
+    @Res() response,
+    @Param("lenderId") lenderId: string
+  ) {
+    try {
+      const loanData = await this.loanService.findLenderApplications(lenderId);
+      return response.status(HttpStatus.OK).json({
+        message: "All loan data found successfullyy",
         loanData,
       });
     } catch (err) {
